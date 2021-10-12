c806dc8ccb59f1ad0a7d3402440d52fc3286345b
Aantriksh Sood <aantrikshsood@gmail.com>
2021-10-12 18:20:13 +0530

    ? Major bug fixes and additions

---
+++ index.js
@@ -0,0 +1,88 @@
+const Discord = require("discord.js");
+const client = new Discord.Client({ intents: 7753 });
+const fs = require("fs");
+const config = require("./config.json");
+client.config = config;
+
+// Initialise discord giveaways
+const { GiveawaysManager } = require("discord-giveaways");
+client.giveawaysManager = new GiveawaysManager(client, {
+  storage: "./storage/giveaways.json",
+  default: {
+    botsCanWin: false,
+    embedColor: "#2F3136",
+    reaction: "🎉",
+    lastChance: {
+      enabled: true,
+      content: `🛑 **Last chance to enter** 🛑`,
+      threshold: 5000,
+      embedColor: '#FF0000'
+    }
+  }
+});
+//Coded by ZeroSync on yt
+
+/* Load all events (discord based) */
+
+
+fs.readdir("./events/discord", (_err, files) => {
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    const event = require(`./events/discord/${file}`);
+    let eventName = file.split(".")[0];
+    console.log(`[Event]   ✅  Loaded: ${eventName}`);
+    client.on(eventName, event.bind(null, client));
+    delete require.cache[require.resolve(`./events/discord/${file}`)];
+  });
+});
+
+/* Load all events (giveaways based) */
+
+
+fs.readdir("./events/giveaways", (_err, files) => {
+  files.forEach((file) => {
+    if (!file.endsWith(".js")) return;
+    const event = require(`./events/giveaways/${file}`);
+    let eventName = file.split(".")[0];
+    console.log(`[Event]   🎉 Loaded: ${eventName}`);
+    client.giveawaysManager.on(eventName, (...file) => event.execute(...file, client)), delete require.cache[require.resolve(`./events/giveaways/${file}`)];
+  })
+})
+
+// Let commands be a new collection ( message commands )
+client.commands = new Discord.Collection();
+/* Load all commands */
+fs.readdir("./commands/", (_err, files) => {
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./commands/${file}`);
+    let commandName = file.split(".")[0];
+    client.commands.set(commandName, {
+      name: commandName,
+      ...props
+    });
+    console.log(`[Command] ✅  Loaded: ${commandName}`);
+  });
+});
+
+// let interactions be a new collection ( slash commands  )
+client.interactions = new Discord.Collection();
+// creating an empty array for registering slash commands
+client.register_arr = []
+/* Load all slash commands */
+fs.readdir("./slash/", (_err, files) => {
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./slash/${file}`);
+    let commandName = file.split(".")[0];
+    client.interactions.set(commandName, {
+      name: commandName,
+      ...props
+    });
+    client.register_arr.push(props)
+  });
+});
+
+
+// Login through the client
+client.login(config.token);
