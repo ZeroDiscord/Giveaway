9cef47c660cf23d9624bc305475cbdb8e5517de8
Aim <91464959+Aim2339@users.noreply.github.com>
2022-10-16 20:01:31 +0530

    Show winner count in embed

--- utils/message.js
+++ utils/message.js
@@ -9,7 +9,7 @@ module.exports = {
   drawing:  `Ends: **{timestamp}**`,
   inviteToParticipate: `React with 🎉 to participate!`,
   winMessage: "Congratulations, {winners}! You won **{this.prize}**!",
-  embedFooter: "Giveaways",
+  embedFooter: "{this.winnerCount} winner(s)",
   noWinner: "Giveaway cancelled, no valid participations.",
   hostedBy: "Hosted by: {this.hostedBy}",
   winners: "winner(s)",
