de91478113e8e1c275165a2cac0cdd40dbe1629d
Aim <91464959+Aim2339@users.noreply.github.com>
2022-10-27 20:25:03 +0530

    Update to v14

--- commands/invite.js
+++ commands/invite.js
@@ -1,25 +1,25 @@
-const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 const config = require('../config.json');
 
 module.exports.run = async (client, message, args) => {
-    const row = new MessageActionRow()
+    const row = new ActionRowBuilder()
     .addComponents(
-        new MessageButton()
+        new ButtonBuilder()
         .setLabel(`Invite ${client.user.username}`)
-        .setStyle('LINK')
+        .setStyle(ButtonStyle.Link)
         .setURL(`https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=applications.commands%20bot`),
-        new MessageButton()
+        new ButtonBuilder()
         .setLabel('Support Server')
-        .setStyle('LINK')
+        .setStyle(ButtonStyle.Link)
         .setURL("https://discord.gg/ARu4hr6hJw"),
     )
-    let invite = new MessageEmbed()
+    let invite = new EmbedBuilder()
      .setAuthor({ 
           name: `Invite ${client.user.username}`, 
           iconURL: client.user.displayAvatarURL() 
      })  
     .setTitle("Invite & Support Link!")
-    .setDescription(`Invite ${client.user} to your server today & enjoy seamless giveaways with advvanced features!`)
+    .setDescription(`Invite ${client.user} to your server today & enjoy seamless giveaways with advanced features!`)
     .setColor('#2F3136')
     .setTimestamp()
     .setFooter({
