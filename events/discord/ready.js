8d71d8d02702e030ef1e97d8a2aba78b924784ab
Aim <91464959+Aim2339@users.noreply.github.com>
2022-10-28 18:24:49 +0530

    Fix bot presence

--- events/discord/ready.js
+++ events/discord/ready.js
@@ -1,4 +1,6 @@
 const register = require('../../utils/slashsync');
+const { ActivityType } = require('discord.js');
+
 module.exports = async (client) => {
   await register(client, client.register_arr.map((command) => ({
     name: command.name,
@@ -12,10 +14,9 @@ module.exports = async (client) => {
   console.log(`[ / | Slash Command ] - ✅ Loaded all slash commands!`)
   let invite = `https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=applications.commands%20bot`;
   console.log(`[STATUS] ${client.user.tag} is now online!\n[INFO] Bot by ZeroSync https://www.youtube.com/c/ZeroSync\n[Invite Link] ${invite}`);
-  const activities = [`All Giveaways Running Smoothly`, `ZeroSync on YouTube`];
-  setInterval(() => {
-    let activity = activities[Math.floor(Math.random() * activities.length)];
-    client.user.setActivity(activity, { type: "WATCHING" });
-  }, 20000);
+  client.user.setPresence({
+  activities: [{ name: `ZeroSync on YouTube`, type: ActivityType.Watching }],
+  status: 'online',
+});
 
 };
